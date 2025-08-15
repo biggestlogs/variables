088e342d277f11c360897ba970447744fb8a5a7d
unknown <adamsjos004@warren.k12.in.us>
2025-08-15 07:45:29 -0400

    Initial commit

---
+++ var.py
@@ -0,0 +1,10 @@
+Name='Joseph'
+Age=14
+Favorite_food='pancakes'
+Dream_job='Mechanical Engineer'
+Hobby='gaming'
+print('Name:', Name)
+print('Age:', Age)
+print('Favorite food:', Favorite_food)
+print('hobby:', Hobby)
+print('Dream:', Dream_job)
